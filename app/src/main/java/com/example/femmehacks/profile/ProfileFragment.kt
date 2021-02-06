58c8c4688c8ff3bf2a76d606f196a6596dddface
annabai <anna.bai.6512@gmail.com>
2021-02-06 16:00:35 -0600

    profile fragment

--- app/src/main/java/com/example/femmehacks/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/femmehacks/profile/ProfileFragment.kt
@@ -12,7 +12,7 @@ import androidx.fragment.app.Fragment
 import com.example.femmehacks.R
 import com.example.femmehacks.databinding.FragmentProfileBinding
 
-class ProfileFragment: Fragment(), AdapterView.OnItemSelectedListener {
+class ProfileFragment: Fragment(), AdapterView.OnItemSelectedListener{
     lateinit var binding : FragmentProfileBinding
 
     override fun onCreateView(
@@ -25,19 +25,34 @@ class ProfileFragment: Fragment(), AdapterView.OnItemSelectedListener {
             R.layout.fragment_profile, container, false
         )
 
-//        val spinner: Spinner = findViewById(R.id.spinner)
-//        // Create an ArrayAdapter using the string array and a default spinner layout
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.relationship_array,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//            // Specify the layout to use when the list of choices appears
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            // Apply the adapter to the spinner
-//            spinner.adapter = adapter
-//        }
+        //SPINNER STUFF
+        val spinner: Spinner = binding.spinner
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        this.context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.relationship_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spinner.adapter = adapter
+            }
+        }
 
         return binding.root
     }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
+
+
+
 }
