15b7eec4cc5efd67c35a5a664b2e6f5a4f7b3834
annabai <anna.bai.6512@gmail.com>
2021-02-06 13:45:20 -0600

    changes to profile fragment

--- app/src/main/java/com/example/femmehacks/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/femmehacks/profile/ProfileFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.femmehacks.R
 import com.example.femmehacks.databinding.FragmentProfileBinding
 
-class ProfileFragment: Fragment() {
+class ProfileFragment: Fragment(), AdapterView.OnItemSelectedListener {
     lateinit var binding : FragmentProfileBinding
 
     override fun onCreateView(
@@ -21,6 +24,19 @@ class ProfileFragment: Fragment() {
             inflater,
             R.layout.fragment_profile, container, false
         )
+
+//        val spinner: Spinner = findViewById(R.id.spinner)
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.relationship_array,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//            // Specify the layout to use when the list of choices appears
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            // Apply the adapter to the spinner
+//            spinner.adapter = adapter
+//        }
 
         return binding.root
     }
