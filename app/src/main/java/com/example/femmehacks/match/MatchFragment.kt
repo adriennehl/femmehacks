6e5f89ed3b6cfb3bf0b9d1be4efde60c6cafa8ba
Janet Lu <janet.lu2019@gmail.com>
2021-02-06 13:05:41 -0600

    Login

--- app/src/main/java/com/example/femmehacks/match/MatchFragment.kt
+++ app/src/main/java/com/example/femmehacks/match/MatchFragment.kt
@@ -10,6 +10,10 @@ import androidx.fragment.app.Fragment
 import com.example.femmehacks.R
 import com.example.femmehacks.databinding.FragmentMatchBinding
 
+import com.firebase.ui.auth.AuthUI
+//import com.google.firebase.quickstart.auth.R
+
+
 class MatchFragment: Fragment() {
     lateinit var binding : FragmentMatchBinding
 
@@ -22,7 +26,21 @@ class MatchFragment: Fragment() {
             inflater,
             R.layout.fragment_match, container, false
         )
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())
 
+                    // Create and launch sign-in intent
+                    startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+            RC_SIGN_IN)
         return binding.root
+    }
+    companion object {
+
+        private const val RC_SIGN_IN = 123
     }
 }
