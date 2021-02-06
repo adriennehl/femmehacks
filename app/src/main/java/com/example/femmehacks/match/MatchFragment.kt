e2a7494ce1b9dd7802ebc7cd188c606a60680b2f
Adrienne <adriennehl36@gmail.com>
2021-02-06 08:34:23 -0600

    basic nav

---
+++ app/src/main/java/com/example/femmehacks/match/MatchFragment.kt
@@ -0,0 +1,28 @@
+package com.example.femmehacks.match
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.example.femmehacks.R
+import com.example.femmehacks.databinding.FragmentMatchBinding
+
+class MatchFragment: Fragment() {
+    lateinit var binding : FragmentMatchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        DataBindingUtil.inflate<ViewDataBinding>(
+            inflater,
+            R.layout.fragment_match, container, false
+        )
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/femmehacks/profile/ProfileFragment.kt
@@ -0,0 +1,27 @@
+package com.example.femmehacks.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.femmehacks.R
+import com.example.femmehacks.databinding.FragmentProfileBinding
+
+class ProfileFragment: Fragment() {
+    lateinit var binding : FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate<FragmentProfileBinding>(
+            inflater,
+            R.layout.fragment_profile, container, false
+        )
+
+        return binding.root
+    }
+}
